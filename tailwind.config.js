31a397e927981831189f46e0dcfa8fbeb258a6b2
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2022-12-12 15:48:42 -0500

    Adding styles and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./*.html'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Barlow Semi Condensed', 'sans-serif'],
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/line-clamp')],
 };
